8ad696dd00b339239cbedc5231cee22a12acd711
IbrahimaLY <mlyibrahima@gmail.com>
2025-03-10 09:18:22 +0000

    fix pre-commit 2

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,3 @@
-
 # Auteur : Ibrahima Oumar LY
 # Date de création : 2025-03-03
 # Description : Ce fichier contient les fonctions pour le jeu Snake.
@@ -9,10 +8,21 @@ ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
 
+def read_high_score():
+    with open("data.txt") as f_score:
+        return int(f_score.read())
+
+
+def write_high_score(score):
+    with open("data.txt", mode="w") as f_score:
+        f_score.write(f"{score}")
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = int(read_high_score())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -20,11 +30,19 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            write_high_score(self.high_score)
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
